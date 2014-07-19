b0998b0c1ccf927e57b48562a5370642dcb6282d
Yurk Sha <yurkins@gmail.com>
2014-07-19 14:50:08 +0300

    fix installing with sudo

--- scripts/install.js
+++ scripts/install.js
@@ -1,9 +1,16 @@
 var path  = require('path'),
     spawn = require('child_process').spawn,
-    module_home = path.join(__dirname, '..');
+    module_home = path.join(__dirname, '..'),
+    spawn_args = [path.join('node_modules', '.bin', 'bower'), 'install', '--config.interactive=false', '--allow-root'];
 
-if (process.getuid && process.getuid() === 0 && process.platform !== 'win32') {
+if (process.platform === 'win32') {
+    spawn_args.unshift('/c');
+    spawn_args.unshift('cmd');
+} else if (!!process.env.npm_config_global) {
+    // if installing globally with `sudo npm -g`
+    // then bower can't get access to its cache located in user profile
+    // because it running with root permissions
     process.env.HOME = path.join(module_home, 'bower_home');
 }
 
-spawn(path.join('node_modules', '.bin', 'bower'), ['install', '--config.interactive=false', '--allow-root'], {cwd: module_home, stdio: 'inherit'});
+spawn(spawn_args.shift(), spawn_args, {cwd: module_home, stdio: 'inherit'});
